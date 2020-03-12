8f4f3002d34831991f96fb6d98707172709e61c2
hammadpirzada1 <hammadpirzada1@gmail.com>
2020-03-12 12:07:15 +0500

    Dashboard Changes

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,21 +4,84 @@
     <div class="row mb-2">
       <div class="col-sm-6">
           <h1 class="m-0 text-dark">Dashboard</h1>
-      </div><!-- /.col -->
-      <!-- <div class="col-sm-6">
-        <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#"></a></li>
-            <li class="breadcrumb-item active"></li>
-        </ol>
-      </div> -->  <!-- /.col -->
-    </div><!-- /.row -->
+      </div>
+    </div>
 @endsection('content_header')
 
-@section('content_body')
-
-    <div class="row">
+<?php 
+  use App\Model\OrderMaster;
+  use App\Model\ProductMaster;
+  use App\User;
       
-    </div>
-    
+?>
+
+@section('content_body')
+        <div class="row">
+
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>{{OrderMaster::count()}}</h3>
+
+                <p>Orders</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <a href="{{route('order.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>{{ProductMaster::count()}}</h3>
+
+                <p>Products</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+              <a href="{{route('product.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>{{User::count()}}</h3>
+
+                <p>Customers</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="{{route('user.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>65</h3>
+
+                <p>Unique Visitors</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-pie-graph"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+
+        </div>
 
 @endsection('content_body')
